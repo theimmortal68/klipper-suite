e8f0789fbdf1df35c1f6fa6db6b7109c25f178c7
Jauschua Stout <jauschua.stout@gmail.com>
2025-08-23 12:39:01 -0400

    Refactor options.sh for improved variable management

--- options.sh
+++ options.sh
@@ -1,39 +1,38 @@
-#!/usr/bin/env bash
-# Default, user-friendly options for local builds and CI.
-# All variables can be overridden via environment or CLI flags (device/profile).
+# Base knobs used by build.sh
+export KS_DEVICE="${KS_DEVICE:-rpi5}"
+export KS_PROFILE="${KS_PROFILE:-base}"
 
-# Suite/arch
 export KS_SUITE="${KS_SUITE:-bookworm}"
 export KS_ARCH="${KS_ARCH:-arm64}"
-export KS_VARIANT="${KS_VARIANT:-apt}"
 export KS_COMPONENTS="${KS_COMPONENTS:-main}"
 export KS_MIRROR="${KS_MIRROR:-http://deb.debian.org/debian}"
+export KS_VARIANT="${KS_VARIANT:-apt}"
 
-# Output/layout
-export KS_OUT_DIR="${KS_OUT_DIR:-out}"
-export KS_IMG_NAME="${KS_IMG_NAME:-debian.img}"
+# Leave empty if you don’t want to force extra packages from base layer
+# (layers can add their own packages cleanly)
+export KS_PACKAGES="${KS_PACKAGES:-}"
+
+export KS_IMG_NAME="${KS_IMG_NAME:-ks-${KS_DEVICE}-${KS_PROFILE}-${KS_SUITE}-${KS_ARCH}.img}"
+export KS_PART_TABLE="${KS_PART_TABLE:-dos}"
 export KS_IMG_SIZE_MIB="${KS_IMG_SIZE_MIB:-4096}"
 export KS_BOOT_SIZE_MIB="${KS_BOOT_SIZE_MIB:-256}"
-export KS_PART_TABLE="${KS_PART_TABLE:-gpt}"
 export KS_BOOT_LABEL="${KS_BOOT_LABEL:-BOOT}"
 export KS_ROOTFS_LABEL="${KS_ROOTFS_LABEL:-rootfs}"
 
-# Device/profile selection (default to a generic rpi-like device layout)
-export KS_DEVICE="${KS_DEVICE:-rpi5}"
-export KS_PROFILE="${KS_PROFILE:-base}"
 export KS_HOSTNAME="${KS_HOSTNAME:-raspi}"
-
-# Locale/timezone/keyboard
+export KS_TIMEZONE="${KS_TIMEZONE:-America/New_York}"
+export KS_LOCALE="${KS_LOCALE:-en_US.UTF-8}"
+export KS_LOCALE_GEN="${KS_LOCALE_GEN:-en_US.UTF-8 UTF-8}"
+export KS_LOCALE_DEFAULT="${KS_LOCALE_DEFAULT:-en_US.UTF-8}"
 export KS_TZ_AREA="${KS_TZ_AREA:-America}"
 export KS_TZ_CITY="${KS_TZ_CITY:-New_York}"
-export KS_TIMEZONE="${KS_TIMEZONE:-${KS_TZ_AREA}/${KS_TZ_CITY}}"
-export KS_LOCALE_DEFAULT="${KS_LOCALE_DEFAULT:-en_US.UTF-8}"
-export KS_LOCALE_GEN="${KS_LOCALE_GEN:-en_US.UTF-8 UTF-8}"
-export KS_LOCALE="${KS_LOCALE:-en_US.UTF-8}"
 export KS_KB_KEYMAP="${KS_KB_KEYMAP:-us}"
 
-# Default user (plaintext; chpasswd will hash inside the chroot)
 export KS_CREATE_USER="${KS_CREATE_USER:-1}"
 export KS_DEVICE_USER="${KS_DEVICE_USER:-pi}"
+export KS_USER_SHELL="${KS_USER_SHELL:-/bin/bash}"
+export KS_USER_GROUPS="${KS_USER_GROUPS:-sudo,adm,video,plugdev,audio,netdev}"
 export KS_USER_PASSWORD_PLAIN="${KS_USER_PASSWORD_PLAIN:-raspberry}"
-export KS_US_
+export KS_PASSWORD_CRYPT_METHOD="${KS_PASSWORD_CRYPT_METHOD:-}"
+
+export KS_OUT_DIR="${KS_OUT_DIR:-out}"
