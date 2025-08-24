3cee14aca68755069e55713e154fa64ac08bb717
Jauschua Stout <jauschua.stout@gmail.com>
2025-08-24 13:18:31 -0400

    Update RPi5 to prepare for stage0, part2

--- klipper-suite-master/image/gpt/ab_userdata/post-build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -eu
-
-rootfs="$1"
-
-# post-build image layout specific ops can be added here
--- klipper-suite-master/image/pre-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -u
-
-if ksconf isset image_pmap ; then
-   [[ -d "${KSIMAGE}/device" ]] || die "No device directory for pmap $KSconf_image_pmap"
-   [[ -f "${KSIMAGE}/device/provisionmap-${KSconf_image_pmap}.json" ]] || die "pmap not found"
-fi
